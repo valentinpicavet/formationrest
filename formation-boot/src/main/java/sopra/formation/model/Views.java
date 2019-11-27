0bd44a8d6276fe9e1e5106e5843740a02ab6c307
Valentin <vapicavet@laposte.net>
2019-11-27 13:58:18 +0100

    views

--- formation-boot/src/main/java/sopra/formation/model/Views.java
+++ formation-boot/src/main/java/sopra/formation/model/Views.java
@@ -1,13 +1,49 @@
 package sopra.formation.model;
 
 public class Views {
-	public static class ViewCommon {}
-	
-	public static class ViewEvaluation extends ViewCommon {}
-	
-	public static class ViewEvaluationDetail extends ViewEvaluation {}
-	
-	public static class ViewStagiaire extends ViewCommon {}
-	
-	public static class ViewStagiaireDetail extends ViewStagiaire {}
+	public static class ViewCommon {
+	}
+
+	public static class ViewEvaluation extends ViewCommon {
+	}
+
+	public static class ViewEvaluationDetail extends ViewEvaluation {
+	}
+
+	public static class ViewStagiaire extends ViewCommon {
+	}
+
+	public static class ViewStagiaireDetail extends ViewStagiaire {
+	}
+
+	public static class ViewSalle extends ViewCommon {
+	}
+
+	public static class ViewModule extends ViewCommon {
+	}
+
+	public static class ViewModuleDetail extends ViewModule {
+	}
+
+	public static class ViewMatiere extends ViewCommon {
+	}
+
+	public static class ViewMatiereDetail extends ViewMatiere {
+	}
+
+	public static class ViewFormateur extends ViewCommon {
+	}
+
+	public static class ViewFormateurFiliere extends ViewCommon {
+	}
+
+	public static class ViewFormateurModule extends ViewCommon {
+	}
+
+	public static class ViewFiliere extends ViewCommon {
+	}
+
+	public static class ViewFormateurCompetence extends ViewCommon {
+	}
+
 }
